1f496ae6fdaa0e7fbb50c3078e44f27c2dc45143
Piotr Kopron <piotr.kopron_s@protonmail.com>
2023-05-14 15:23:40 +0200

    extra comment for Point.types

--- app/src/main/java/msdoilspill/Point.java
+++ app/src/main/java/msdoilspill/Point.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Point{
 	
 	public ArrayList<Point> neighbors;
+	//Required by GUI (jak jest menu wyboru typu to stąd bierze opcje)
 	public static Integer []types ={0,1,2,3};
 	public int type;
 	public int staticField;
