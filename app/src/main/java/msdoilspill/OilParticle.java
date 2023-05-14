fb338d26d04991b395033fdc37b70628339d5c36
Piotr Kopron <piotr.kopron_s@protonmail.com>
2023-05-14 21:30:55 +0200

    extra comments

--- app/src/main/java/msdoilspill/OilParticle.java
+++ app/src/main/java/msdoilspill/OilParticle.java
@@ -46,7 +46,7 @@ public class OilParticle {
         int y = location_y_m / CEV.cellSize_m;
         if( x >= 0 && y>=0 && x < Board.getInstance().cells.length && y < Board.getInstance().cells[0].length)
             return Board.getInstance().cells[x][y];
-        //out of AO, wa can stop modeling this particle.
+        //out of AO. FIXME: Right now oil decays on the borders, without actually leaving the AO. Intended?
         return null;
     }
 }
