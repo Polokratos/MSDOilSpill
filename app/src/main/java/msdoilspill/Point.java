2224c178c7f2b5438b6d93a340c8eb687911ace6
Piotr Kopron <piotr.kopron_s@protonmail.com>
2023-05-14 15:18:27 +0200

    Point cleanup from arifacts of lab6

--- app/src/main/java/msdoilspill/Point.java
+++ app/src/main/java/msdoilspill/Point.java
@@ -1,12 +1,8 @@
 package msdoilspill;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
 public class Point{
-
-	private static final int SFMAX = 100000;
 	
 	public ArrayList<Point> neighbors;
 	public static Integer []types ={0,1,2,3};
@@ -15,22 +11,12 @@ public class Point{
 	public boolean isPedestrian;
 
 	public Point() {
-		type=0;
-		staticField = SFMAX;
-		neighbors= new ArrayList<Point>();
 	}
 	
 	public void clear() {
-		staticField = SFMAX;
-		
 	}
 
-	public boolean calcStaticField() {		
-		return false;
-	}
-	
 	public void move(){
-	
 	}
 
 	public void addNeighbor(Point nei) {
